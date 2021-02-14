b49cbb0368f902703af22aff3eb8bc8954f6f385
Умар <45699126+Godikbot@users.noreply.github.com>
2021-02-14 07:24:58 +0500

    Update info.py

--- handlers/local_commands/info.py
+++ handlers/local_commands/info.py
@@ -1,6 +1,6 @@
 import subprocess
 
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 
 async def info(*_) -> str:
@@ -8,9 +8,9 @@ async def info(*_) -> str:
     out = subprocess.run("git log origin/master -1 --pretty=format:%B",
                          shell=True, capture_output=True).stdout
     out = out.decode('utf-8').splitlines()
-    update_info = 'Доступно обновление! Новая версия: ' + out[0] + '\n'
+    update_info = 'Новая версия: ' + out[0] + '\n'
     if len(out) > 1:
         update_info += 'Что нового:\n' + '\n'.join(out[2:]) + '\n\n'
     if out[0] == VERSION:
         update_info = ''
-    return f"{update_info}LP модуль ver. {VERSION}\nБольше инфы в следующих сериях"  # noqa
+    return f"{update_info}Бета лп ver. {VERSION}\nБольше инфы в следующем обновление"  # noqa
